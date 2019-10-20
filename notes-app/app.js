07fdf4b92b1f5bd194f303808c49defaa6584549
Siddharth-H <siddharthahatkar@gmail.com>
2019-10-20 13:01:53 -0600

    npm init and used validator module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
-const name = require("./util.js")
+const validator = require('validator')
 const getNotes = require("./notes.js")
 
-console.log(name())
-console.log(getNotes())
+const msg = getNotes()
+
+console.log(msg)
+
+console.log(validator.isURL('https://sid.com'))
